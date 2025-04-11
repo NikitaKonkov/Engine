b106ced2c182e53275691aea2c561508fd6f01e1
NikitaKonkov <nikita.wagner97@gmail.com>
2025-04-11 20:18:49 +0100

    minor changes

--- build.bat
+++ build.bat
@@ -1,15 +1,37 @@
 @echo off
 echo Building Engine project...
 
+REM Parse command line arguments for AVX options
+set USE_AVX512=OFF
+set CLEAN_BUILD=false
+
+:parse_args
+if "%1"=="" goto done_args
+if /i "%1"=="--avx512" set USE_AVX512=ON
+if /i "%1"=="--avx2" set USE_AVX512=OFF
+if /i "%1"=="--clean" set CLEAN_BUILD=true
+shift
+goto parse_args
+:done_args
+
+echo AVX512 is %USE_AVX512%
+
 REM Create build directory if it doesn't exist
 if not exist build mkdir build
+
+REM Clean build if requested
+if "%CLEAN_BUILD%"=="true" (
+    echo Cleaning build directory...
+    rmdir /s /q build
+    mkdir build
+)
 
 REM Navigate to build directory
 cd build
 
 REM Configure with CMake
 echo Configuring with CMake...
-"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -G "Visual Studio 17 2022" -T host=x86 -A x64 ..\Engine
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DUSE_AVX512=%USE_AVX512% -G "Visual Studio 17 2022" -T host=x86 -A x64 ..\Engine
 
 REM Build the project
 echo Building project...
@@ -25,6 +47,12 @@ echo Build successful!
 
 REM Run the executable
 echo Running the application...
-Release\mygame.exe
+Release\gameengine.exe
 
 echo Done!
+
+
+REM build.bat --avx2       # Build with AVX2 (default)
+REM build.bat --avx512     # Build with AVX512
+REM build.bat --clean      # Clean build with default AVX2
+REM build.bat --clean --avx512  # Clean build with AVX512
