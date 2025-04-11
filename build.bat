6159f2df88d06109f008947b71f21c8f6eb75d91
NikitaKonkov <nikita.wagner97@gmail.com>
2025-04-11 21:26:22 +0100

    refactor: update build script and CMake configuration for AVX options

--- build.bat
+++ build.bat
@@ -3,24 +3,32 @@ echo Building Engine project...
 
 REM Parse command line arguments for AVX options
 set USE_AVX512=OFF
-set CLEAN_BUILD=false
+set CLEAN_BUILD=OFF
+
+REM Set the path to CMake executable
+set CMAKE_PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe"
 
 :parse_args
 if "%1"=="" goto done_args
 if /i "%1"=="--avx512" set USE_AVX512=ON
 if /i "%1"=="--avx2" set USE_AVX512=OFF
-if /i "%1"=="--clean" set CLEAN_BUILD=true
+if /i "%1"=="--clean" set CLEAN_BUILD=ON
 shift
 goto parse_args
 :done_args
 
-echo AVX512 is %USE_AVX512%
+echo [BUILD INFO] Using instruction set: %USE_AVX512%
+if "%USE_AVX512%"=="ON" (
+    echo [BUILD INFO] AVX-512 instructions will be enabled
+) else (
+    echo [BUILD INFO] AVX-2 instructions will be used (default)
+)
 
 REM Create build directory if it doesn't exist
 if not exist build mkdir build
 
 REM Clean build if requested
-if "%CLEAN_BUILD%"=="true" (
+if "%CLEAN_BUILD%" == "ON" (
     echo Cleaning build directory...
     rmdir /s /q build
     mkdir build
@@ -31,11 +39,11 @@ cd build
 
 REM Configure with CMake
 echo Configuring with CMake...
-"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DUSE_AVX512=%USE_AVX512% -G "Visual Studio 17 2022" -T host=x86 -A x64 ..\Engine
+%CMAKE_PATH% -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DUSE_AVX512=%USE_AVX512% -G "Visual Studio 17 2022" -T host=x86 -A x64 ..\Engine
 
 REM Build the project
 echo Building project...
-"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" --build . --config Release
+%CMAKE_PATH% --build . --config Release
 
 REM Check if build was successful
 if %ERRORLEVEL% NEQ 0 (
