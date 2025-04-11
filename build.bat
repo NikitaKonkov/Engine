e8512b22a8a346adfde194814aee408628509068
NikitaKonkov <nikita.wagner97@gmail.com>
2025-04-11 01:41:18 +0100

    work

---
+++ build.bat
@@ -0,0 +1,30 @@
+@echo off
+echo Building Engine project...
+
+REM Create build directory if it doesn't exist
+if not exist build mkdir build
+
+REM Navigate to build directory
+cd build
+
+REM Configure with CMake
+echo Configuring with CMake...
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -G "Visual Studio 17 2022" -T host=x86 -A x64 ..\cmakeSetup-master
+
+REM Build the project
+echo Building project...
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" --build . --config Release
+
+REM Check if build was successful
+if %ERRORLEVEL% NEQ 0 (
+    echo Build failed with error code %ERRORLEVEL%
+    exit /b %ERRORLEVEL%
+)
+
+echo Build successful!
+
+REM Run the executable
+echo Running the application...
+Release\mygame.exe
+
+echo Done!
