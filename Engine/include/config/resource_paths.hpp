804d74aa27285d25b419406cf79f1aa9a144e77f
NikitaKonkov <nikita.wagner97@gmail.com>
2025-04-12 04:29:17 +0100

    refactor: remove obsolete shader file paths from resource_paths.hpp

--- Engine/include/config/resource_paths.hpp
+++ Engine/include/config/resource_paths.hpp
@@ -8,9 +8,6 @@ namespace Config {
     const std::string GRAPHICS_CONFIG_FILE = "resources/video_settings.txt";
     const std::string KEYBOARD_CONFIG_FILE = "resources/keyboard_config.txt";
     
-    // Shader files
-    const std::string VERTEX_SHADER_PATH = "resources/shaders/shape_vert.spv";
-    const std::string FRAGMENT_SHADER_PATH = "resources/shaders/shape_frag.spv";
     
     // Add more resource paths as needed
     
